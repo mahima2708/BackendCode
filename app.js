8940e7c796ac63dbb42348cc726e0c0413b3c0df
mahima2708 <mhtamta5@gmail.com>
2023-03-03 22:13:21 +0530

    server req and res

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-console.log('mahima');
+if(req.url==='/home'){
+    res.setHeader('content-Type', 'text/plain');
+    res.write('Welcome home');
+    res.end();
+}
+else if(req.url==='/about'){
+    res.setHeader('content-Type', 'text/plain');
+    res.write('Welcome to about us page');
+    res.end();
+}
+else if(req.url ==='/node'){
+    res.setHeader('content-Type', 'text/plain');
+    res.write('Welcome to my Node Js project');
+    res.end();
+}
+else if(req.url==='/'){
+    res.setHeader('content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Server</title></head>');
+    res.write('<body><h1>Hello From My First Server</h1></body>');
+    res.write('</html>');
+    res.end();
+}
+
+
 });
 
 server.listen(4000);

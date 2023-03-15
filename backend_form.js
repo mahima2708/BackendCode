66bfbb140fd3e213f446ef5b4c481361bb2a01c4
mahima2708 <mhtamta5@gmail.com>
2023-03-15 16:49:37 +0530

    express framework

--- backend_form.js
+++ backend_form.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
-const route = require('./routes');
-//const server = http.careteServer(route);
+const express =  require('express');
 
+const apps = express();
 
-const server = http.createServer(route.handler);
+apps.use((req, res, next) => {
+    console.log('in th emiddleware here');
+    next();
+});
 
-server.listen(4001);
+apps.use((req, res, next) => {
+    console.log('in another middleware here');
+    res.send('<h1>hello from express</h1>');
+   
+});
+
+apps.listen(3000);

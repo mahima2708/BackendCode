f5d11c474102869b7de36d33eff813e46e025937
mahima2708 <mhtamta5@gmail.com>
2023-03-16 15:45:06 +0530

    body parser

--- backend_form.js
+++ backend_form.js
@@ -2,17 +2,25 @@ const http = require('http');
 
 const express =  require('express');
 
-const apps = express();
+const bodyParser= require('body-parser');
 
-apps.use((req, res, next) => {
-    console.log('in th emiddleware here');
-    next();
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product',(req, res, next) => {
+ res.send('<form action="/product" method="POST"> <input type="text" name="title"><br><input type="text" name="size"><button type="submit">Submit</button> </form>');
+   
+});
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
 });
 
-apps.use((req, res, next) => {
+
+app.use('/',(req, res, next) => {
     console.log('in another middleware here');
     res.send('<h1>hello from express</h1>');
    
 });
 
-apps.listen(3000);
+app.listen(3000);
